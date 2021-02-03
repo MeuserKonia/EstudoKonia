9f10e946c77fbe5ce502322b4216a196b2588eba
Thiago Meuser <thiago.meuser@konia.com.br>
2021-02-03 12:23:19 -0300

    commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KoniaEstudoWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
